2b7aaf959d696bc8efc6bf38ec2ea1e8a099468a
shiva <shiva.nanda366@gmail.com>
2026-09-22 01:57:12 +0530

    main-1

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,7 @@ export const Header = () => {
   console.log(24);
   console.log("hello world");
   console.log(22);
+  console.log("main", 1);
 
   useEffect(() => {
     localStorage.setItem("theme", JSON.stringify(theme));
