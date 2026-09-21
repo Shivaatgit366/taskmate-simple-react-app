838531edff6bdfabc237dfb9b09da4cb897276df
shiva <shiva.nanda366@gmail.com>
2026-09-22 01:55:36 +0530

    2

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,9 @@ export const Header = () => {
   console.log(24);
   console.log("hello world");
   console.log(22);
+  console.log(1);
   console.log("main", 1);
+  console.log(2);
   console.log("main", 2);
   console.log("main", 3);
   console.log("main", 4);
