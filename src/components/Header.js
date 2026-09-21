095d340fa4ae81b9ee5e516531a02c24159fb04e
shiva <shiva.nanda366@gmail.com>
2026-09-21 23:04:25 +0530

    added log

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import Logo from "../assets/logo.png"
 export const Header = () => {
   const [theme, setTheme] = useState(JSON.parse(localStorage.getItem("theme")) || "medium");
   console.log("conflict should come here", theme);
+  console.log(22);
 
   useEffect(() => {
     localStorage.setItem("theme", JSON.stringify(theme));
